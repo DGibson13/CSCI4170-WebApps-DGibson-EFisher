81d10bbe8ff3df72e85b73391a6eacd9172053ab
David Gibson <david.gibson41@gmail.com>
2022-04-27 16:37:07 -0400

    add sort function, fix radio button on import

--- Final-Project/questlog/__init__.py
+++ Final-Project/questlog/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("DROP TABLE IF EXISTS games")       
-        cur.execute("CREATE TABLE IF NOT EXISTS games (title varchar PRIMARY KEY, favorite integer DEFAULT 0, logo varchar, genre varchar, hours integer, completion integer, notes varchar)")
+        cur.execute("CREATE TABLE IF NOT EXISTS games (title varchar PRIMARY KEY NOT NULL, favorite integer DEFAULT 0, logo varchar, genre varchar, hours integer, completion integer, notes varchar)")
         cur.execute("INSERT INTO games (title, logo, genre, hours, completion) VALUES (%s, %s, %s, %s, %s)",('Elden Ring', 'https://quiviracoalition.org/wp-content/uploads/2019/02/generic-person-icon.png', 'RPG', 250, 0))
         cur.execute("INSERT INTO games (title, favorite, logo, genre, hours, completion) VALUES (%s, %s, %s, %s, %s, %s)",('Dark Souls', 0, 'logo.com', 'RPG', 250, 1))
         cur.execute("INSERT INTO games (title, favorite, logo, genre, hours, completion) VALUES (%s, %s, %s, %s, %s, %s)",('Donkey Kong', 1, 'logo.com', 'RPG', 250, 2))
@@ -28,13 +28,13 @@ def create_app(test_config=None):
         return
 
     # a simple page that says hello
-    @app.route('/', methods=["GET", "POST"])
+    @app.route('/', methods=["GET"])
     def index():
         #reset_test_table()
 
         conn = get_db_connection()
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS games (title varchar PRIMARY KEY, favorite integer DEFAULT 0, logo varchar, genre varchar, hours integer, completion integer, notes varchar)") 
+        cur.execute("CREATE TABLE IF NOT EXISTS games (title varchar PRIMARY KEY NOT NULL, favorite integer DEFAULT 0, logo varchar, genre varchar, hours integer, completion integer, notes varchar)") 
         
         cur.execute("SELECT * FROM games")
         games = cur.fetchall()
@@ -58,13 +58,19 @@ def create_app(test_config=None):
             logo = request.form['logo']
             genre = request.form['genre']
             hours = request.form['hours']
+            completion = request.form['playing']
+
+            if logo is None:
+                logo = "url_for('static', filename='noimage.png')"
 
            
-            cur.execute("INSERT INTO games (title, logo, genre, hours) VALUES (%s, %s, %s, %s)",(title, logo, genre, hours))
+            cur.execute("INSERT INTO games (title, logo, genre, hours, completion) VALUES (%s, %s, %s, %s, %s)",(title, logo, genre, hours, completion))
+            #cur.execute("INSERT INTO games (title, logo, genre, hours) VALUES (%s, %s, %s, %s)",(title, logo, genre, hours))
             conn.commit()
 
             cur.close()
             conn.close()
+            return redirect('/')
 
         return render_template('import.html')
 
@@ -76,6 +82,7 @@ def create_app(test_config=None):
         title = request.form.get('title')
         favorite = request.form.get('favorite')
         notes = request.form.get('notes')
+        #playing = request.form.get('playing')
         if favorite is None:
             favorite = 0
         
@@ -85,5 +92,70 @@ def create_app(test_config=None):
         cur.close()
         conn.close()
         return redirect('/')
+
+    @app.route('/sortTitle', methods=["GET"])
+    def sortTitle():
+        conn = get_db_connection()
+        cur = conn.cursor()        
+
+        cur.execute("SELECT * FROM games ORDER BY title ASC")
+        games = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('index.html', games=games)
+
+    @app.route('/sortFavorite', methods=["GET"])
+    def sortFavorite():
+        conn = get_db_connection()
+        cur = conn.cursor()        
+
+        cur.execute("SELECT * FROM games ORDER BY favorite DESC")
+        games = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('index.html', games=games)
+
+    @app.route('/sortHours', methods=["GET"])
+    def sortHours():
+        conn = get_db_connection()
+        cur = conn.cursor()        
+
+        cur.execute("SELECT * FROM games ORDER BY hours ASC")
+        games = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('index.html', games=games)
+
+    @app.route('/sortGenre', methods=["GET"])
+    def sortGenre():
+        conn = get_db_connection()
+        cur = conn.cursor()        
+
+        cur.execute("SELECT * FROM games ORDER BY genre DESC")
+        games = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('index.html', games=games)
+    
+    @app.route('/sortCompletion', methods=["GET"])
+    def sortCompletion():
+        conn = get_db_connection()
+        cur = conn.cursor()        
+
+        cur.execute("SELECT * FROM games ORDER BY completion DESC")
+        games = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return render_template('index.html', games=games)
 
     return app
